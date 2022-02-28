27134c6454808478635d732f088ce7106a9e53cc
Alexandre Ribeiro de Oliveira <alexandre.programador84@gmail.com>
2022-02-28 18:16:14 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuarioAPI.DTO;
+
+namespace UsuarioAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Cadastra([FromBody] CreateUsuarioDTO dto ) 
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Profiles/UsuarioProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using UsuarioAPI.DTO;
+using UsuarioAPI.Model;
+
+namespace UsuarioAPI.Profiles
+{
+
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDTO, Usuario>();
+        }
+
+    }
+}
